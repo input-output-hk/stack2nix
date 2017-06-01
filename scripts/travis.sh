3397955a4e2c9bf42e87c0ef0b639479fb2544d5
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-06-01 14:04:37 -0700

    Simplify cabal2nix-2.2.1 installation

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,16 +6,8 @@ set -ex
 nix-env -i stack nix-prefetch-git git
 
 # install cabal2nix 2.2.1
-CABAL2NIX_DIR=$(mktemp -d)
-git clone https://github.com/NixOS/cabal2nix "$CABAL2NIX_DIR"
-pushd "$CABAL2NIX_DIR"
-git checkout b6834fd420e0223d0d57f8f98caeeb6ac088be88
 PATH="$HOME/.local/bin:$PATH"
-stack setup
-stack init
-stack build
-stack install
-popd
+stack install cabal2nix-2.2.1
 which cabal2nix
 cabal2nix --version
 
