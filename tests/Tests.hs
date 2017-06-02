1d81d5339ffbde479516cb5f5d9ae477acef8d89
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-06-01 20:44:53 -0700

    Replace golden tests with nix expression verification tests

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,30 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Test.Tasty
-import           Test.Tasty.Golden
 import           Test.Tasty.HUnit
 import           Test.Tasty.Program
 
-import           Control.Monad         (when)
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as BSC
-import           System.Directory      (createDirectoryIfMissing, doesPathExist,
-                                        removePathForcibly)
 import           System.Environment    (setEnv)
-import           System.FilePath       (takeDirectory, takeFileName, (<.>),
-                                        (</>))
+import           System.FilePath       (takeFileName)
+import           System.IO.Temp        (withSystemTempDirectory)
 import           System.Process
 
 import           Stack2nix
 
+nixPath :: String
 nixPath = "nixpkgs=https://github.com/NixOS/nixpkgs/archive/21a8239452adae3a4717772f4e490575586b2755.tar.gz"
+
 
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests, programTests, goldenTests]
+tests = testGroup "Tests" [unitTests, programTests, nixExpressionTests]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
@@ -70,31 +68,19 @@ programTests = testGroup "executable exit code tests"
   [ testProgram "supports --help" "stack2nix" ["--help"] Nothing
   ]
 
-goldenTests :: TestTree
-goldenTests = testGroup "executable output tests"
+nixExpressionTests :: TestTree
+nixExpressionTests = testGroup "nix output tests"
   [ nixFile "https://github.com/jmitchell/haskell-dummy-project1.git" "7e7d91d86ba0f86633ab37279c013879ade09e32" "haskell-dummy-package1" "default.nix"
   , nixFile "https://github.com/jmitchell/haskell-multi-package-demo1.git" "e3d9bd6d6066dab5222ce53fb7d234f28eafa2d5" "haskell-multi-proj-demo1" "default.nix"
   , nixFile "https://github.com/input-output-hk/cardano-sl.git" "be7cb65f71e7bd5b34778652009469c4513ecb79" "cardano-sl" "default.nix"
   ]
   where
-    shouldVerifyBuild = False
-
     nixFile proj rev targetAttr nixFname =
-      let
-        nickname = takeFileName proj
-        description = "generates usable " ++ nixFname ++ " for " ++ nickname
-        goldFile = "./tests/gold" </> nickname </> nixFname <.> "golden"
-        -- TODO: replace /tmp with portable solution
-        outDir = "/tmp" </> nickname
-        actualFile = outDir </> nixFname
-        diffCmd = \ref new -> ["diff", "-u", ref, new]
-        task = do
+      testCaseSteps ("generates usable " ++ nixFname ++ " for " ++ takeFileName proj) $ \step ->
+        withSystemTempDirectory "s2n-tests" $ \outDir -> do
+          step "Running stack2nix to generate nix expression"
           setEnv "NIX_PATH" nixPath
-          createDirectoryIfMissing True $ takeDirectory goldFile
-          doesPathExist outDir >>= \exists -> when exists $ removePathForcibly outDir
           readCreateProcess (proc "stack2nix" ["--outdir", outDir, "--revision", rev, proj]) "" >>= putStrLn
-          if shouldVerifyBuild
-            then readCreateProcess (proc "nix-build" ["-A", targetAttr]){ cwd = Just outDir } "" >> return ()
-            else return ()
-      in
-        goldenVsFileDiff description diffCmd goldFile actualFile task
+
+          step "Verifying generated nix expression builds"
+          readCreateProcess (proc "nix-build" ["-A", targetAttr]){ cwd = Just outDir } "" >> return ()
