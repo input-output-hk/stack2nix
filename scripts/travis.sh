a6fdaeb120a1f66f4bed2f1923fe4142ff6231bd
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-06-05 15:29:58 -0700

    Explicitly use --system-ghc and migrate tests to shell script.
    My global stack configuration on my nixos machine had been set to use
    --system-ghc. After deleting everything in $STACK_ROOT it was clear
    stack2nix needs this. Now, as @deepfire suggested, all calls to stack
    from stack2nix use --system-ghc, and therefore the CLI option for it
    has been removed.
    
    The Haskell tests have been replaced by smoke tests at the end of
    ./scripts/travis.sh. Stack runs in a context with its own set of
    environment variables, so `stack test` is prone to failure. Even after
    manually patching the PATH in Tests.hs to include ~/.local/bin and
    ~/.nix-profile/bin, calls to stack2nix from the tests were failing
    with an encoding error while reading a file handle. Calling stack2nix
    with the same arguments from the shell works, and that's the case we
    actually care about.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,16 +2,30 @@
 
 set -ex
 
+# set up local environment
+PATH="$HOME/.local/bin:$HOME/.nix-profile/bin:$PATH"
+NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs/archive/21a8239452adae3a4717772f4e490575586b2755.tar.gz"
+
 # install dependencies from nix
-nix-env -i stack nix-prefetch-git git cabal-install
+nix-env -i stack nix-prefetch-git git cabal-install ghc
 
 # install cabal2nix 2.2.1
-PATH="$HOME/.local/bin:$PATH"
-stack install --install-ghc cabal2nix-2.2.1
+stack --nix --system-ghc install cabal2nix-2.2.1
 which cabal2nix
 cabal2nix --version
 
-# run stack2nix tests
+# build and install
 cabal update
-stack setup
-stack test
+stack --nix --system-ghc setup
+stack --nix --system-ghc install
+
+# smoke tests
+stack2nix -o /tmp/haskell-dummy-project1.nix \
+	  --revision 7e7d91d86ba0f86633ab37279c013879ade09e32 \
+	  https://github.com/jmitchell/haskell-dummy-project1.git
+nix-build -A haskell-dummy-package1 /tmp/haskell-dummy-project1.nix
+
+stack2nix -o /tmp/haskell-multi-package-demo1.nix \
+	  --revision e3d9bd6d6066dab5222ce53fb7d234f28eafa2d5 \
+	  https://github.com/jmitchell/haskell-multi-package-demo1.git
+nix-build -A haskell-multi-proj-demo1 /tmp/haskell-multi-package-demo1.nix
--- stack2nix/Main.hs
+++ stack2nix/Main.hs
@@ -8,7 +8,6 @@ args :: Parser Args
 args = Args
        <$> optional (strOption $ long "revision" <> help "revision to use when fetching from VCS")
        <*> optional (strOption $ short 'o' <> help "output file for generated nix expression")
-       <*> (flag False True $ long "system-ghc" <> help "pass --system-ghc to stack invocations")
        <*> strArgument (metavar "URI")
 
 main :: IO ()
