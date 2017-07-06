44569a47890b789dc92b3923870b68d86666e5cc
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-06 19:01:05 +0300

    runCmdFrom:  print all command invocations to stderr

--- src/Stack2nix/External/Util.hs
+++ src/Stack2nix/External/Util.hs
@@ -1,14 +1,18 @@
 module Stack2nix.External.Util where
 
+import           Data.List
+import           Data.Monoid
 import           System.Directory (getCurrentDirectory)
 import           System.Exit      (ExitCode (..))
 import           System.Process   (CreateProcess (..))
 import           System.Process   (proc, readCreateProcessWithExitCode)
+import qualified System.IO               as Sys
 
 type CmdResult = (Bool, String, String)
 
 runCmdFrom :: FilePath -> String -> [String] -> IO CmdResult
 runCmdFrom dir prog args = do
+  Sys.hPutStrLn Sys.stderr $ "runCmdFrom '" <> dir <> "' '" <> prog <> " " <> show (intercalate " " args)
   (exitCode, stdout, stderr) <- readCreateProcessWithExitCode (fromDir dir (proc prog args)) ""
   case exitCode of
     ExitSuccess -> return $ (True,  stdout, stderr)
