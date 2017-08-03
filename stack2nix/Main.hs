f4af381706b3d744078543da09c48b7ac4939587
Domen Kožar <domen@enlambda.com>
2017-08-03 20:31:32 +0200

    Default to 4 threads since most of our work is IO bound (#44)
    With the release of Nix 1.11.12 we don't have concurrent
    builtins.fetchTarball problem anymore, so for now let's
    assume a reasonable default is 4 threads and see how it
    plays out.

--- stack2nix/Main.hs
+++ stack2nix/Main.hs
@@ -9,7 +9,7 @@ args :: Parser Args
 args = Args
        <$> optional (strOption $ long "revision" <> help "revision to use when fetching from VCS")
        <*> optional (strOption $ short 'o' <> help "output file for generated nix expression" <> metavar "PATH")
-       <*> option auto (short 'j' <> help "number of threads for subprocesses" <> showDefault <> value 1 <> metavar "INT")
+       <*> option auto (short 'j' <> help "number of threads for subprocesses" <> showDefault <> value 4 <> metavar "INT")
        <*> switch (long "test" <> help "enable tests")
        <*> switch (long "haddock" <> help "enable documentation generation")
        <*> strArgument (metavar "URI")
