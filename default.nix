83aa25dc3694bafe309c437f28ba3e8eff99c953
Domen Kožar <domen@dev.si>
2017-08-09 14:31:25 +0200

    stack2nix.nix: support macOS

--- default.nix
+++ default.nix
@@ -2,4 +2,16 @@
 
 with (import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; } ); 
 
-justStaticExecutables (import ./stack2nix.nix { inherit pkgs; }).stack2nix
+((import ./stack2nix.nix { inherit pkgs; }).override {
+  overrides = self: super: {
+    stack2nix = justStaticExecutables super.stack2nix;
+
+    # https://github.com/NixOS/cabal2nix/issues/146
+    hinotify = if pkgs.stdenv.isDarwin then self.hfsevents else super.hinotify;
+    # Darwin fixes upstreamed in nixpkgs commit 71bebd52547f4486816fd320bb3dc6314f139e67
+    hfsevents = self.callPackage ./hfsevents.nix { inherit (pkgs.darwin.apple_sdk.frameworks) Cocoa CoreServices; };
+    fsnotify = if pkgs.stdenv.isDarwin
+      then addBuildDepend (dontCheck super.fsnotify) pkgs.darwin.apple_sdk.frameworks.Cocoa
+      else dontCheck super.fsnotify;
+  };
+}).stack2nix
