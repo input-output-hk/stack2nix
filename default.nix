c4462413e790904eeeb2bb48f7f1b3d3d1417c81
Domen Kožar <domen@dev.si>
2017-08-23 22:33:14 +0200

    Really use Cabal 2.0.0.2

--- default.nix
+++ default.nix
@@ -6,6 +6,9 @@ with pkgs.haskell.lib;
   overrides = self: super: {
     stack2nix = justStaticExecutables super.stack2nix;
 
+    # https://github.com/input-output-hk/stack2nix/issues/51
+    Cabal = pkgs.haskell.packages.ghc802.Cabal_2_0_0_2;
+
     # https://github.com/NixOS/cabal2nix/issues/146
     hinotify = if pkgs.stdenv.isDarwin then self.hfsevents else super.hinotify;
     # Darwin fixes upstreamed in nixpkgs commit 71bebd52547f4486816fd320bb3dc6314f139e67
