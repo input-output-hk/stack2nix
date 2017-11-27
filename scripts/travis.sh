343cca388502acb35285aa2232cdaa3580970302
Domen Kožar <domen@dev.si>
2017-11-27 22:43:42 +0000

    Simplify CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,22 +1,21 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -p cabal2nix stack nix-prefetch-git git cabal-install ghc -i bash
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/a905b7cd0c2dc0714195a50bf176cd8e4593502d.tar.gz
+#!/usr/bin/env bash
 
 readlink=$(nix-instantiate --eval -E "/. + (import <nix/config.nix>).coreutils")/readlink
 scriptDir=$(dirname -- "$($readlink -f -- "${BASH_SOURCE[0]}")")
-export NIX_PATH="nixpkgs=$(nix-build "${scriptDir}/../fetch-nixpkgs.nix" --no-out-link)"
+NIXPKGS=$(nix-build "${scriptDir}/../fetch-nixpkgs.nix" --no-out-link)
+export NIX_PATH="nixpkgs=$NIXPKGS"
 
 
 set -ex
 
+STACK=$(nix-build -A stack $NIXPKGS)/bin
+NIX_PREFETCH=$(nix-build -A nix-prefetch-git $NIXPKGS)/bin
+GIT=$(nix-build -A git $NIXPKGS)/bin
+CABAL_INSTALL=$(nix-build -A cabal-install $NIXPKGS)/bin
 # set up local environment
-PATH="$HOME/.local/bin:$PATH"
-
-# install cabal2nix 2.2.1
-cabal2nix --version
+PATH="$HOME/.local/bin:$STACK:$NIX_PREFETCH:$GIT:$CABAL_INSTALL:$PATH"
 
 # build and install
-stack --nix --system-ghc setup
 stack --nix --system-ghc install --fast --ghc-options="+RTS -A128m -n2m -RTS"
 stack --nix --system-ghc test --ghc-options="+RTS -A128m -n2m -RTS"
 
