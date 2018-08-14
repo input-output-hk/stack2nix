88fd8be0cad55e4f29575c5d55645f6321201c17
Domen Kožar <domen@dev.si>
2018-08-14 12:03:16 +0100

    cleanup default.nix

--- default.nix
+++ default.nix
@@ -6,6 +6,7 @@ with pkgs.haskell.lib;
   overrides = self: super: {
     # TODO: separate out output
     stack2nix = justStaticExecutables super.stack2nix;
+
     # https://github.com/commercialhaskell/lts-haskell/issues/149
     stack = doJailbreak super.stack;
 
@@ -14,10 +15,5 @@ with pkgs.haskell.lib;
 
     # https://github.com/NixOS/cabal2nix/issues/146
     hinotify = if pkgs.stdenv.isDarwin then self.hfsevents else super.hinotify;
-    # Darwin fixes upstreamed in nixpkgs commit 71bebd52547f4486816fd320bb3dc6314f139e67
-    hfsevents = self.callPackage ./hfsevents.nix { inherit (pkgs.darwin.apple_sdk.frameworks) Cocoa CoreServices; };
-    fsnotify = if pkgs.stdenv.isDarwin
-      then addBuildDepend (dontCheck super.fsnotify) pkgs.darwin.apple_sdk.frameworks.Cocoa
-      else dontCheck super.fsnotify;
   };
 }).stack2nix
